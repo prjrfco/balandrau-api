f0de24c5d38239195c930223b56dfb55551dac4b
Francisco <francisco.junior@ipdec.org>
2023-09-22 13:54:15 -0400

    adicionado logs de inicialização

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './common/env-values';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Bootstrap');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(envs.APP_PORT);
+  await app.listen(envs.APP_PORT, () => {
+    logger.log(`Server running on port: ${envs.APP_PORT} ...`);
+  });
 }
+
 bootstrap().then(() => {});
